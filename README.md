56ea068d881875cd051171104f58707b410a8764
Aubrey Portwood <code@aubreypwd.com>
2021-11-26 10:46:59 -0700

    First commit; version I used in .zshrc for a bit.

--- README.md
+++ README.md
@@ -1,24 +1,82 @@
-# ``
+# `newvwp`
 
-Description
+Spins up a new WordPress site using Valet. 
+
+If you run this in an un-parked directory, it will prompt you to park the directory. It will then create the directory, install WordPress, setup constants for you, and optionally install `spatie/ray` and `debug-bar-*` plugins for you.
+
+## Constants
+
+```
+DISABLE_WP_CRON true 
+EMPTY_TRASH_DAYS 0 
+WP_MAX_MEMORY_LIMIT 4096 
+WP_MEMORY_LIMIT 4096 
+COMPRESS_CSS false 
+COMPRESS_SCRIPTS false 
+CONCATENATE_SCRIPTS false 
+ENFORCE_GZIP false 
+FS_METHOD direct
+WP_DEBUG true 
+SCRIPT_DEBUG true 
+WP_DEBUG_DISPLAY false 
+WP_DEBUG_LOG true 
+FORCE_SSL_ADMIN true
+FORCE_SSL_LOGIN true
+SAVE_QUERIES true 
+```
+
+## Debug Bar & Development plugins (`--debug-bar`)
+
+- `debug-bar`
+- `debug-bar-console`
+- `debug-bar-shortcodes`
+- `debug-bar-constants`
+- `debug-bar-post-types`
+- `debug-bar-cron`
+- `debug-bar-actions-and-filters-addon`
+- `debug-bar-transients`
+- `debug-bar-list-dependencies`
+- `debug-bar-remote-requests`
+- `query-monitor`
+
+## Username/Password for Login
+
+- Username: `admin`
+- Password: `password`
+
+## MySQL
+
+This assumes you have `mysql` installed and uses `127.0.0.1` as `dbhost`, and `root` as `dbuser` w/out a password.
+
+It will attempt to create the database for you, but if it exists the install steps will fail!
 
 ## Usage
 
 ```bash
+newvwp --spatie/ray --mailhog --debug-bar
 ```
 
-## Requires
+### Flags
 
-- `...`<sup>*</sup>
+```
+--spatie/ray Installs https://spatie.be/products/ray support using Composer.
+--mailhog    Installs wp-mailhog-smtp plugin for Mailhog support.
+--debug-bar  Installs all debug-bar-* plugins and activates them.
 
-<sup>*</sup> If using [homebrew](https://brew.sh) on macOS, these will automatically be installed.
+```
+
+## Required Commands
+
+- `valet`
+- `composer`
+- `wp`
 
 ## Install
 
 Using [antigen](https://github.com/zsh-users/antigen):
 
 ```bash
-antigen bundle aubreypwd/zsh-plugin-NAME@1.0.0
+antigen bundle aubreypwd/zsh-plugin-newvwp@1.0.0
 ```
 
 ## Development
@@ -26,10 +84,10 @@ antigen bundle aubreypwd/zsh-plugin-NAME@1.0.0
 Install the package on `master`:
 
 ```bash
-antigen bundle aubreypwd/zsh-plugin-NAME
+antigen bundle ssh://git@github.com/aubreypwd/zsh-plugin-newvwp
 ```
 
-...and contribute upstream by working in `$HOME/.antigen/bundles/aubreypwd/zsh-plugin-NAME`.
+...and contribute upstream by working in `$HOME/.antigen/bundles/aubreypwd/zsh-plugin-newvwp`.
 
 ---
 
@@ -37,4 +95,4 @@ antigen bundle aubreypwd/zsh-plugin-NAME
 
 ### 1.0.0
 
-- First version
+- First version built for _Five for the Future_; thanks [@WebDevStudios](https://webdevstudios.com/)
--- zsh-plugin-NAME.plugin.zsh
+++ zsh-plugin-NAME.plugin.zsh
@@ -1,19 +1,132 @@
 #!/bin/zsh
 
-if [[ $(command -v antigen) ]]; then
-	antigen bundle aubreypwd/zsh-plugin-require@1.0.1
-	antigen apply
-
-	require "{BREWCOMMAND}" "brew reinstall {BREWCOMMAND}" "brew"
-fi
-
-if ! [[ -x $(command -v {BREWCOMMAND}) ]]; then
-	echo "Please install {BREWCOMMAND} to use {NAME}."
-	return;
-fi
-
 ###
- #
- #
- # @since 1.0.0
- ##
+	 # Create a new WordPress site using Valet.
+	 #
+	 # E.g: newvwp example
+	 #
+	 # @since Friday, November 26, 2021
+	 #
+	 # @TODO Move this to a ZSH plugin.
+	 ##
+	function newvwp {
+
+		if ! [[ -x $(command -v valet) ]]; then
+			echo "Unable to find valet command, do you have it installed and in your PATH?"
+			return;
+		fi
+
+		if ! [[ -x $(command -v wp) ]]; then
+			echo "Please install wp-cli, as it is required to install plugins and setup WordPress."
+			return;
+		fi
+
+		if [[ "$@" == *"spatie/ray"* ]]; then
+			if ! [[ -x $(command -v composer) ]]; then
+				echo "Please install composer, as it is required to require spatie/ray."
+				return;
+			fi
+		fi
+
+		if [[ -z "$1" ]]; then
+			newvwp --help
+			return
+		fi
+
+		if [[ "$1" == '--help' ]]; then
+			echo "Please specify a site name, e.g.:"
+			echo
+			echo "newvwp example [--flags]"
+			echo
+			echo " --spatie/ray Installs https://spatie.be/products/ray support using Composer."
+			echo " --mailhog    Installs wp-mailhog-smtp plugin for Mailhog support."
+			echo " --debug-bar  Installs all debug-bar-* plugins and activates them."
+
+			return
+		fi
+
+		pwd=$(pwd)
+
+		if read -q "choice?Are you sure you want to park ${pwd} and create ./$1? [y/n]: "; then
+			echo
+			echo
+
+			echo "You may be asked for your password to park ${pwd}..."
+				echo
+
+				# Park the current directory so that the folder we make inside can be served.
+				valet park && \
+
+					# Make the new site in this parked directory.
+					mkdir -p "$1" && \
+					cd "$1" && \
+
+			echo "You may be asked for your password to secure '$1.test'..." && \
+				valet secure && \
+
+					echo && \
+					echo "Installing WordPress in ${pwd}/$1..." && \
+						wp core download && \
+						wp config create --dbuser=root --dbhost="127.0.0.1" --dbname="$1" && \
+						wp db create && \
+						wp core install --url="https://$1.test" --title="$1" --admin_user="admin" --admin_email="noreply@example.com" && \
+
+						echo && \
+						echo "Changing admin user's password to 'password' instead..." && \
+							wp user update admin --user_pass=password && \
+
+								echo && \
+								echo "Setting up constants..." && \
+									wp config set DISABLE_WP_CRON true --raw && \
+									wp config set EMPTY_TRASH_DAYS 0 --raw && \
+									wp config set WP_MAX_MEMORY_LIMIT 4096 --raw && \
+									wp config set WP_MEMORY_LIMIT 4096 --raw && \
+									wp config set COMPRESS_CSS false --raw && \
+									wp config set COMPRESS_SCRIPTS false --raw && \
+									wp config set CONCATENATE_SCRIPTS false --raw && \
+									wp config set ENFORCE_GZIP false --raw && \
+									wp config set FS_METHOD direct && \
+									wp config set WP_DEBUG true --raw && \
+									wp config set SCRIPT_DEBUG true --raw && \
+									wp config set WP_DEBUG_DISPLAY false --raw && \
+									wp config set WP_DEBUG_LOG true --raw && \
+									wp config set FORCE_SSL_ADMIN true --raw && \
+									wp config set FORCE_SSL_LOGIN true --raw && \
+									wp config set SAVE_QUERIES true --raw
+
+			if [[ "$@" == *"spatie/ray"* ]]; then
+				echo
+				echo "Setting up spatie/ray..."
+					composer init --name="no/installable" -q -n && \
+						composer require spatie/ray && \
+							wp config set '__SPATIE_RAY' "require __DIR__ . '/vendor/autoload.php'" --raw --type='variable'
+			fi
+
+			if [[ "$@" == *"--debug-bar"* ]]; then
+				echo
+				echo "Installing debug-bar-* plugins..."
+					wp plugin install --activate debug-bar
+					wp plugin install --activate debug-bar-console
+					wp plugin install --activate debug-bar-shortcodes
+					wp plugin install --activate debug-bar-constants
+					wp plugin install --activate debug-bar-post-types
+					wp plugin install --activate debug-bar-cron
+					wp plugin install --activate debug-bar-actions-and-filters-addon
+					wp plugin install --activate debug-bar-transients
+					wp plugin install --activate debug-bar-list-dependencies
+					wp plugin install --activate debug-bar-remote-requests
+					wp plugin install --activate query-monitor
+			fi
+
+			if [[ "$@" == *"--mailhog"* ]]; then
+				echo
+				echo "Installing wp-mailhot-smtp..."
+					wp plugin install --activate wp-mailhog-smtp
+			fi
+
+			echo
+			echo "Done - https://$1.test/wp-login.php"
+				echo "\tUsername: admin"
+				echo "\tPassword: password"
+		fi
+	}
