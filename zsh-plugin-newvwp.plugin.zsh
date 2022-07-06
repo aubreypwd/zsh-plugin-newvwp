70fbd9897ed6eff9d3e855159655ed8560c0384a
Aubrey Portwood <code@aubreypwd.com>
2022-07-06 15:18:47 -0600

    Make these independant commands (functions) the user can use.

--- zsh-plugin-newvwp.plugin.zsh
+++ zsh-plugin-newvwp.plugin.zsh
@@ -14,11 +14,10 @@
 		###
 		 # Help for newvwp.
 		 #
-		 # E.g: Not for use.
-		 #
 		 # @since Friday, November 26, 2021
 		 ##
-		function newvwp_help {
+		function newvwp-help {
+
 			echo "Please specify a site name, e.g.:"
 			echo
 			echo "newvwp example [--flags]"
@@ -26,8 +25,90 @@
 			echo " --spatie/ray Installs https://spatie.be/products/ray support using Composer."
 			echo " --mailhog    Installs wp-mailhog-smtp plugin for Mailhog support."
 			echo " --debug-bar  Installs all debug-bar-* plugins and activates them."
+			echo
+			echo "Other commands:"
+			echo
+			echo "set-debug-constats      (Will just set the constants)"
+			echo "install-mailhog-plugin  (Will just install the plugin for Mailhog)"
+			echo "install-spatie-ray-wp   (Will just require spatie/ray and load it in wp-config.php)"
 
 			return
+		}
+
+		###
+		 # Setup debug constants.
+		 #
+		 # @since Wednesday, July 6, 2022
+		 ##
+		function set-debug-constants {
+
+			wp config set BP_DEFAULT_COMPONENT 'staging-area'
+			wp config set BP_XPROFILE_SLUG 'staging-area'
+			wp config set COMPRESS_CSS false --raw
+			wp config set COMPRESS_SCRIPTS false --raw
+			wp config set CONCATENATE_SCRIPTS false --raw
+			wp config set DISABLE_WP_CRON true --raw
+			wp config set EMPTY_TRASH_DAYS 0 --raw
+			wp config set ENFORCE_GZIP false --raw
+			wp config set EP_DASHBOARD_SYNC false --raw
+			wp config set EP_HOST 'http://failed.tld/'
+			wp config set FORCE_SSL_ADMIN true --raw
+			wp config set FORCE_SSL_LOGIN true --raw
+			wp config set FS_CHMOD_DIR 0775 --raw
+			wp config set FS_CHMOD_FILE 0664 --raw
+			wp config set FS_METHOD 'direct'
+			wp config set JETPACK_DEV_DEBUG true --raw
+			wp config set SAVE_QUERIES true --raw
+			wp config set SCRIPT_DEBUG true --raw
+			wp config set WP_AUTO_UPDATE_CORE false --raw
+			wp config set WP_CACHE false --raw
+			wp config set WP_DEBUG true --raw
+			wp config set WP_DEBUG_DISPLAY false --raw
+			wp config set WP_DEBUG_LOG true --raw
+			wp config set WP_ENVIRONMENT_TYPE 'local'
+			wp config set WP_LOCAL_DEV true --raw
+			wp config set WP_MAX_MEMORY_LIMIT 4096 --raw
+			wp config set WP_MEMORY_LIMIT 4096 --raw
+		}
+
+		###
+		 # Install debugging plugins.
+		 #
+		 # @since Wednesday, July 6, 2022
+		 ##
+		function install-debug-plugins {
+
+			wp plugin install --activate debug-bar
+			wp plugin install --activate debug-bar-console
+			wp plugin install --activate debug-bar-shortcodes
+			wp plugin install --activate debug-bar-constants
+			wp plugin install --activate debug-bar-post-types
+			wp plugin install --activate debug-bar-cron
+			wp plugin install --activate debug-bar-actions-and-filters-addon
+			wp plugin install --activate debug-bar-transients
+			wp plugin install --activate debug-bar-list-dependencies
+			wp plugin install --activate debug-bar-remote-requests
+			wp plugin install --activate query-monitor
+		}
+
+		###
+		 # Require spatie ray and configure WordPress to use it.
+		 #
+		 # @since Wednesday, July 6, 2022
+		 ##
+		function install-spatie-ray-wp {
+			composer init --name="no/installable" -q -n && \
+				composer require spatie/ray && \
+					wp config set '__SPATIE_RAY' "require __DIR__ . '/vendor/autoload.php'" --raw --type='variable'
+		}
+
+		###
+		 # Install mailhog.
+		 #
+		 # @since Wednesday, July 6, 2022
+		 ##
+		function install-mailhog-plugin {
+			wp plugin install --activate wp-mailhog-smtp
 		}
 
 		if ! [[ -x $(command -v valet) ]]; then
@@ -40,7 +121,7 @@
 			return;
 		fi
 
-		if [[ "$@" == *"spatie/ray"* ]]; then
+		if [[ "$@" == *"--spatie/ray"* ]]; then
 			if ! [[ -x $(command -v composer) ]]; then
 				echo "Please install composer, as it is required to require spatie/ray."
 				return;
@@ -48,15 +129,15 @@
 		fi
 
 		if [[ -z "$1" ]]; then
-			newvwp_help && return
+			newvwp-help && return
 		fi
 
 		if [[ "$1" == *"--"* ]]; then
-			newvwp_help && return
+			newvwp-help && return
 		fi
 
 		if [[ "$1" == '--help' ]]; then
-			newvwp_help && return
+			newvwp-help && return
 		fi
 
 		local pwd=$(pwd)
@@ -91,62 +172,24 @@
 
 								echo && \
 								echo "Setting up constants..." && \
-									wp config set BP_DEFAULT_COMPONENT 'staging-area' && \
-									wp config set BP_XPROFILE_SLUG 'staging-area' && \
-									wp config set COMPRESS_CSS false --raw && \
-									wp config set COMPRESS_SCRIPTS false --raw && \
-									wp config set CONCATENATE_SCRIPTS false --raw && \
-									wp config set DISABLE_WP_CRON true --raw && \
-									wp config set EMPTY_TRASH_DAYS 0 --raw && \
-									wp config set ENFORCE_GZIP false --raw && \
-									wp config set EP_DASHBOARD_SYNC false --raw && \
-									wp config set EP_HOST 'http://failed.tld/' && \
-									wp config set FORCE_SSL_ADMIN true --raw && \
-									wp config set FORCE_SSL_LOGIN true --raw && \
-									wp config set FS_CHMOD_DIR 0775 --raw && \
-									wp config set FS_CHMOD_FILE 0664 --raw && \
-									wp config set FS_METHOD 'direct' && \
-									wp config set JETPACK_DEV_DEBUG true --raw && \
-									wp config set SAVE_QUERIES true --raw && \
-									wp config set SCRIPT_DEBUG true --raw && \
-									wp config set WP_AUTO_UPDATE_CORE false --raw && \
-									wp config set WP_CACHE false --raw && \
-									wp config set WP_DEBUG true --raw && \
-									wp config set WP_DEBUG_DISPLAY false --raw && \
-									wp config set WP_DEBUG_LOG true --raw && \
-									wp config set WP_ENVIRONMENT_TYPE 'local' && \
-									wp config set WP_LOCAL_DEV true --raw && \
-									wp config set WP_MAX_MEMORY_LIMIT 4096 --raw && \
-									wp config set WP_MEMORY_LIMIT 4096 --raw
+									set-debug-constants
 
 			if [[ "$@" == *"--spatie/ray"* ]]; then
 				echo
 				echo "Setting up spatie/ray..."
-					composer init --name="no/installable" -q -n && \
-						composer require spatie/ray && \
-							wp config set '__SPATIE_RAY' "require __DIR__ . '/vendor/autoload.php'" --raw --type='variable'
+					install-spatie-ray-wp
 			fi
 
 			if [[ "$@" == *"--debug-bar"* ]]; then
 				echo
 				echo "Installing debug-bar-* plugins..."
-					wp plugin install --activate debug-bar
-					wp plugin install --activate debug-bar-console
-					wp plugin install --activate debug-bar-shortcodes
-					wp plugin install --activate debug-bar-constants
-					wp plugin install --activate debug-bar-post-types
-					wp plugin install --activate debug-bar-cron
-					wp plugin install --activate debug-bar-actions-and-filters-addon
-					wp plugin install --activate debug-bar-transients
-					wp plugin install --activate debug-bar-list-dependencies
-					wp plugin install --activate debug-bar-remote-requests
-					wp plugin install --activate query-monitor
+					install-debug-plugins
 			fi
 
 			if [[ "$@" == *"--mailhog"* ]]; then
 				echo
 				echo "Installing wp-mailhot-smtp..."
-					wp plugin install --activate wp-mailhog-smtp
+					install-mailhog-plugin
 			fi
 
 			echo
