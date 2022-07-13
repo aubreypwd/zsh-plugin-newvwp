f668e4cd85151b175b49e405521a055ab109d1a9
Aubrey Portwood <code@aubreypwd.com>
2022-07-13 09:18:29 -0600

    Disable email by default.

--- zsh-plugin-newvwp.plugin.zsh
+++ zsh-plugin-newvwp.plugin.zsh
@@ -1,6 +1,120 @@
 #!/bin/zsh
 
-###
+	###
+	 # Help for newvwp.
+	 #
+	 # @since Friday, November 26, 2021
+	 ##
+	function newvwp-help {
+
+		echo "Please specify a site name, e.g.:"
+		echo
+		echo "newvwp example [--flags]"
+		echo
+		echo " --spatie/ray Installs https://spatie.be/products/ray support using Composer."
+		echo " --mailhog    Installs wp-mailhog-smtp plugin for Mailhog support (if you don't use this wp_mail() will be disabled by default)."
+		echo " --debug-bar  Installs all debug-bar-* plugins and activates them."
+		echo
+		echo "Other commands:"
+		echo
+		echo "set-debug-constats      (Will just set the constants)"
+		echo "install-mailhog-plugin  (Will just install the plugin for Mailhog)"
+		echo "install-spatie-ray-wp   (Will just require spatie/ray and load it in wp-config.php)"
+		echo "disable-wp-mail         (Will disable wp_mail() in wp-config.php)"
+
+		return
+	}
+
+	###
+	 # Disable wp_mail().
+	 #
+	 # @since Wednesday, July 13, 2022
+	 ##
+	function disable-wp-mail {
+
+		echo "Disabling wp_mail in wp-config.php" && \
+			echo "<?php function wp_mail() { return false; } @ini_set( 'disable_functions','mail' ); // This should disable all emails." > "disable-wp-mail.php" && \
+				wp config set '__DISABLE_WP_MAIL' "require __DIR__ . '/disable-wp-mail.php'" --raw --type='variable'
+	}
+
+	###
+	 # Setup debug constants.
+	 #
+	 # @since Wednesday, July 6, 2022
+	 ##
+	function set-debug-constants {
+
+		echo
+		echo "Setting up constants..."
+
+		wp config set BP_DEFAULT_COMPONENT 'staging-area'
+		wp config set BP_XPROFILE_SLUG 'staging-area'
+		wp config set COMPRESS_CSS false --raw
+		wp config set COMPRESS_SCRIPTS false --raw
+		wp config set CONCATENATE_SCRIPTS false --raw
+		wp config set DISABLE_WP_CRON true --raw
+		wp config set EMPTY_TRASH_DAYS 0 --raw
+		wp config set ENFORCE_GZIP false --raw
+		wp config set EP_DASHBOARD_SYNC false --raw
+		wp config set EP_HOST 'http://failed.tld/'
+		wp config set FORCE_SSL_ADMIN true --raw
+		wp config set FORCE_SSL_LOGIN true --raw
+		wp config set FS_CHMOD_DIR 0775 --raw
+		wp config set FS_CHMOD_FILE 0664 --raw
+		wp config set FS_METHOD 'direct'
+		wp config set JETPACK_DEV_DEBUG true --raw
+		wp config set SAVE_QUERIES true --raw
+		wp config set SCRIPT_DEBUG true --raw
+		wp config set WP_AUTO_UPDATE_CORE false --raw
+		wp config set WP_CACHE false --raw
+		wp config set WP_DEBUG true --raw
+		wp config set WP_DEBUG_DISPLAY false --raw
+		wp config set WP_DEBUG_LOG true --raw
+		wp config set WP_ENVIRONMENT_TYPE 'local'
+		wp config set WP_LOCAL_DEV true --raw
+		wp config set WP_MAX_MEMORY_LIMIT 4096 --raw
+		wp config set WP_MEMORY_LIMIT 4096 --raw
+	}
+
+	###
+	 # Install debugging plugins.
+	 #
+	 # @since Wednesday, July 6, 2022
+	 ##
+	function install-debug-plugins {
+
+		echo
+		echo "Installing debug-bar-* plugins..."
+
+		wp plugin install --activate debug-bar
+		wp plugin install --activate debug-bar-console
+		wp plugin install --activate debug-bar-shortcodes
+		wp plugin install --activate debug-bar-constants
+		wp plugin install --activate debug-bar-post-types
+		wp plugin install --activate debug-bar-cron
+		wp plugin install --activate debug-bar-actions-and-filters-addon
+		wp plugin install --activate debug-bar-transients
+		wp plugin install --activate debug-bar-list-dependencies
+		wp plugin install --activate debug-bar-remote-requests
+		wp plugin install --activate query-monitor
+	}
+
+	###
+	 # Require spatie ray and configure WordPress to use it.
+	 #
+	 # @since Wednesday, July 6, 2022
+	 ##
+	function install-spatie-ray-wp {
+
+		echo
+		echo "Setting up spatie/ray..."
+
+		composer init --name="no/installable" -q -n && \
+			composer require spatie/ray && \
+				wp config set '__SPATIE_RAY' "require __DIR__ . '/vendor/autoload.php'" --raw --type='variable'
+	}
+
+	###
 	 # Create a new WordPress site using Valet.
 	 #
 	 # E.g: newvwp example
@@ -12,102 +126,15 @@
 	function newvwp {
 
 		###
-		 # Help for newvwp.
-		 #
-		 # @since Friday, November 26, 2021
-		 ##
-		function newvwp-help {
-
-			echo "Please specify a site name, e.g.:"
-			echo
-			echo "newvwp example [--flags]"
-			echo
-			echo " --spatie/ray Installs https://spatie.be/products/ray support using Composer."
-			echo " --mailhog    Installs wp-mailhog-smtp plugin for Mailhog support."
-			echo " --debug-bar  Installs all debug-bar-* plugins and activates them."
-			echo
-			echo "Other commands:"
-			echo
-			echo "set-debug-constats      (Will just set the constants)"
-			echo "install-mailhog-plugin  (Will just install the plugin for Mailhog)"
-			echo "install-spatie-ray-wp   (Will just require spatie/ray and load it in wp-config.php)"
-
-			return
-		}
-
-		###
-		 # Setup debug constants.
-		 #
-		 # @since Wednesday, July 6, 2022
-		 ##
-		function set-debug-constants {
-
-			wp config set BP_DEFAULT_COMPONENT 'staging-area'
-			wp config set BP_XPROFILE_SLUG 'staging-area'
-			wp config set COMPRESS_CSS false --raw
-			wp config set COMPRESS_SCRIPTS false --raw
-			wp config set CONCATENATE_SCRIPTS false --raw
-			wp config set DISABLE_WP_CRON true --raw
-			wp config set EMPTY_TRASH_DAYS 0 --raw
-			wp config set ENFORCE_GZIP false --raw
-			wp config set EP_DASHBOARD_SYNC false --raw
-			wp config set EP_HOST 'http://failed.tld/'
-			wp config set FORCE_SSL_ADMIN true --raw
-			wp config set FORCE_SSL_LOGIN true --raw
-			wp config set FS_CHMOD_DIR 0775 --raw
-			wp config set FS_CHMOD_FILE 0664 --raw
-			wp config set FS_METHOD 'direct'
-			wp config set JETPACK_DEV_DEBUG true --raw
-			wp config set SAVE_QUERIES true --raw
-			wp config set SCRIPT_DEBUG true --raw
-			wp config set WP_AUTO_UPDATE_CORE false --raw
-			wp config set WP_CACHE false --raw
-			wp config set WP_DEBUG true --raw
-			wp config set WP_DEBUG_DISPLAY false --raw
-			wp config set WP_DEBUG_LOG true --raw
-			wp config set WP_ENVIRONMENT_TYPE 'local'
-			wp config set WP_LOCAL_DEV true --raw
-			wp config set WP_MAX_MEMORY_LIMIT 4096 --raw
-			wp config set WP_MEMORY_LIMIT 4096 --raw
-		}
-
-		###
-		 # Install debugging plugins.
-		 #
-		 # @since Wednesday, July 6, 2022
-		 ##
-		function install-debug-plugins {
-
-			wp plugin install --activate debug-bar
-			wp plugin install --activate debug-bar-console
-			wp plugin install --activate debug-bar-shortcodes
-			wp plugin install --activate debug-bar-constants
-			wp plugin install --activate debug-bar-post-types
-			wp plugin install --activate debug-bar-cron
-			wp plugin install --activate debug-bar-actions-and-filters-addon
-			wp plugin install --activate debug-bar-transients
-			wp plugin install --activate debug-bar-list-dependencies
-			wp plugin install --activate debug-bar-remote-requests
-			wp plugin install --activate query-monitor
-		}
-
-		###
-		 # Require spatie ray and configure WordPress to use it.
-		 #
-		 # @since Wednesday, July 6, 2022
-		 ##
-		function install-spatie-ray-wp {
-			composer init --name="no/installable" -q -n && \
-				composer require spatie/ray && \
-					wp config set '__SPATIE_RAY' "require __DIR__ . '/vendor/autoload.php'" --raw --type='variable'
-		}
-
-		###
 		 # Install mailhog.
 		 #
 		 # @since Wednesday, July 6, 2022
 		 ##
 		function install-mailhog-plugin {
+
+			echo
+			echo "Installing wp-mailhot-smtp..."
+
 			wp plugin install --activate wp-mailhog-smtp
 		}
 
@@ -169,27 +196,20 @@
 						echo && \
 						echo "Changing admin user's password to 'password' instead..." && \
 							wp user update admin --user_pass=password && \
-
-								echo && \
-								echo "Setting up constants..." && \
 									set-debug-constants
 
 			if [[ "$@" == *"--spatie/ray"* ]]; then
-				echo
-				echo "Setting up spatie/ray..."
-					install-spatie-ray-wp
+				install-spatie-ray-wp
 			fi
 
 			if [[ "$@" == *"--debug-bar"* ]]; then
-				echo
-				echo "Installing debug-bar-* plugins..."
-					install-debug-plugins
+				install-debug-plugins
 			fi
 
 			if [[ "$@" == *"--mailhog"* ]]; then
-				echo
-				echo "Installing wp-mailhot-smtp..."
-					install-mailhog-plugin
+				install-mailhog-plugin
+			else
+				disable-wp-mail # Yes, disable email by default.
 			fi
 
 			echo
